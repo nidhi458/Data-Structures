778ed94da40ba132a13da20c63aee3a51c2bd74e
Nidhi D B <nidhidb.cs23@bmsce.ac.in>
2024-10-18 10:58:21 +0530

    Leetcode q1.c

--- Leetcode q1.c
+++ Leetcode q1.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 #define MAX 100
 
-int stack1[MAX], stack2[MAX];
-int top1 = -1, top2 = -1;
-
-
-void insert(int value) {
-    if (top1 == MAX - 1) {
+void insert(int value, int stack1[], int *top1) {
+    if (*top1 == MAX - 1) {
         printf("Queue Overflow\n");
     } else {
-        stack1[++top1] = value;
+        stack1[++(*top1)] = value;
         printf("inserted %d\n", value);
     }
 }
 
-
-int delete() {
-    
-    if (top1 == -1 && top2 == -1) {
+int delete(int stack1[], int *top1, int stack2[], int *top2) {
+    if (*top1 == -1 && *top2 == -1) {
         printf("Queue Underflow\n");
         return -1;
     }
     
-    if (top2 == -1) {
-        while (top1 != -1) {
-            stack2[++top2] = stack1[top1--];
+    if (*top2 == -1) {
+        while (*top1 != -1) {
+            stack2[++(*top2)] = stack1[(*top1)--];
         }
     }
     
-    int value = stack2[top2--];
+    int value = stack2[(*top2)--];
     printf("deleted element: %d\n", value);
     return value;
 }
 
 int main() {
+    int stack1[MAX], stack2[MAX];
+    int top1 = -1, top2 = -1;
     int choice, value;
     
     while (1) {
@@ -48,10 +44,10 @@ int main() {
             case 1:
                 printf("Enter value to enqueue: ");
                 scanf("%d", &value);
-                insert(value);
+                insert(value, stack1, &top1);
                 break;
             case 2:
-                delete();
+                delete(stack1, &top1, stack2, &top2);
                 break;
             case 3:
                 return 0;
